93657ef470a98c79f652e3c6f2ee771ac95fe5b3
Severin Klaus <sk@hinderlingvolkart.com>
2019-12-18 18:29:39 +0100

    Include asNavFor

--- dist/flickity.pkgd.js
+++ dist/flickity.pkgd.js
@@ -3940,50 +3940,6 @@ return Flickity;
 }));
 
 /*!
- * Flickity v2.2.0
- * Modified by Hinderling Volkart
- * Touch, responsive, flickable carousels
- *
- * Licensed GPLv3 for open source use
- * or Flickity Commercial License for commercial use
- *
- * https://flickity.metafizzy.co
- * Copyright 2015-2019 Metafizzy
- */
-
-( function( window, factory ) {
-  // universal module definition
-  /* jshint strict: false */
-  if ( typeof define == 'function' && define.amd ) {
-    // AMD
-    define( 'flickity/js/index',[
-      './flickity',
-      './drag',
-      './prev-next-button',
-      './page-dots',
-      './player',
-      './add-remove-cell',
-      './lazyload'
-    ], factory );
-  } else if ( typeof module == 'object' && module.exports ) {
-    // CommonJS
-    module.exports = factory(
-      require('./flickity'),
-      require('./drag'),
-      require('./prev-next-button'),
-      require('./page-dots'),
-      require('./player'),
-      require('./add-remove-cell'),
-      require('./lazyload')
-    );
-  }
-
-})( window, function factory( Flickity ) {
-  /*jshint strict: false*/
-  return Flickity;
-});
-
-/*!
  * Flickity asNavFor v2.0.1
  * enable asNavFor for Flickity
  */
@@ -4133,6 +4089,51 @@ proto.destroyAsNavFor = function() {
 return Flickity;
 
 }));
+
+/*!
+ * Flickity v2.2.0
+ * Modified by Hinderling Volkart
+ * Touch, responsive, flickable carousels
+ *
+ * Licensed GPLv3 for open source use
+ * or Flickity Commercial License for commercial use
+ *
+ * https://flickity.metafizzy.co
+ * Copyright 2015-2019 Metafizzy
+ */
+
+(function(window, factory) {
+	// universal module definition
+	/* jshint strict: false */
+	if (typeof define == 'function' && define.amd) {
+		// AMD
+		define('flickity/js/index',[
+			'./flickity',
+			'./drag',
+			'./prev-next-button',
+			'./page-dots',
+			'./player',
+			'./add-remove-cell',
+			'./lazyload',
+			'flickity-as-nav-for/as-nav-for'
+		], factory);
+	} else if (typeof module == 'object' && module.exports) {
+		// CommonJS
+		module.exports = factory(
+			require('./flickity'),
+			require('./drag'),
+			require('./prev-next-button'),
+			require('./page-dots'),
+			require('./player'),
+			require('./add-remove-cell'),
+			require('./lazyload'),
+			require('flickity-as-nav-for/as-nav-for')
+		);
+	}
+})(window, function factory(Flickity) {
+	/*jshint strict: false*/
+	return Flickity;
+});
 
 /*!
  * imagesLoaded v4.1.3
--- js/index.js
+++ js/index.js
@@ -10,34 +10,35 @@
  * Copyright 2015-2019 Metafizzy
  */
 
-( function( window, factory ) {
-  // universal module definition
-  /* jshint strict: false */
-  if ( typeof define == 'function' && define.amd ) {
-    // AMD
-    define( [
-      './flickity',
-      './drag',
-      './prev-next-button',
-      './page-dots',
-      './player',
-      './add-remove-cell',
-      './lazyload'
-    ], factory );
-  } else if ( typeof module == 'object' && module.exports ) {
-    // CommonJS
-    module.exports = factory(
-      require('./flickity'),
-      require('./drag'),
-      require('./prev-next-button'),
-      require('./page-dots'),
-      require('./player'),
-      require('./add-remove-cell'),
-      require('./lazyload')
-    );
-  }
-
-})( window, function factory( Flickity ) {
-  /*jshint strict: false*/
-  return Flickity;
+(function(window, factory) {
+	// universal module definition
+	/* jshint strict: false */
+	if (typeof define == 'function' && define.amd) {
+		// AMD
+		define([
+			'./flickity',
+			'./drag',
+			'./prev-next-button',
+			'./page-dots',
+			'./player',
+			'./add-remove-cell',
+			'./lazyload',
+			'flickity-as-nav-for/as-nav-for'
+		], factory);
+	} else if (typeof module == 'object' && module.exports) {
+		// CommonJS
+		module.exports = factory(
+			require('./flickity'),
+			require('./drag'),
+			require('./prev-next-button'),
+			require('./page-dots'),
+			require('./player'),
+			require('./add-remove-cell'),
+			require('./lazyload'),
+			require('flickity-as-nav-for/as-nav-for')
+		);
+	}
+})(window, function factory(Flickity) {
+	/*jshint strict: false*/
+	return Flickity;
 });
